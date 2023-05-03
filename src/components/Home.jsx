6520e7ccb1e085c13b6f0059bbe76ff91946d184
DANNJUGUNA <ndan30071@gmail.com>
2023-05-03 14:26:35 +0300

    updated the home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -7,27 +7,30 @@ import CV from "../assets/dan resume.docx"
 const Home = () => {
  
   return (
-    <div className=' flex-wrap items-center justify-center flex flex-col mt-0'>
-       <p className=' pt-14 mb-6 text-2xl font-semibold text-white'>
+    <div className=' flex flex-wrap'>
+    <div className=' flex-wrap items-center justify-center flex flex-col mt-0 w-[97%]'>
+       <p className=' pt-14 mb-6 text-2xl font-semibold text-black lg:text-white'>
         Hi, I'm
        </p>
-       <p className='mb-6 text-2xl font-semibold text-white'>
+       <p className='mb-6 text-2xl font-semibold text-black lg:text-white'>
         Dan Njuguna
        </p>
-       <p className='mb-6 text-2xl font-semibold text-white'>
+       <p className='mb-6 text-2xl font-semibold text-black lg:text-white'>
         A fullstack developer
        </p>
-       <img className=' w-[400px] h-[400px] rounded-full' src={dan} alt="dan" />
-       <div>
-       <a href={CV}download className='text-white rounded-full p-2 border border-solid border-white'>Download CV</a>
-       <a href="#contacts" className='text-white rounded-full p-2 border border-solid border-white ml-5' >Lets Talk</a>
+       <img className=' w-[300px] h-[300px] lg:w-[400px] lg:h-[400px] rounded-full' src={dan} alt="dan" />
+       <div className=' mt-3'>
+       <a href={CV}download className='text-white rounded-full p-2 border text-lg font-semibold border-solid border-white hover:bg-white hover:text-black ml-14 lg:text-white lg:text-2xl lg:border-white '>Download CV</a>
+       <a href="#contacts" className='text-white rounded-full p-2 border text-lg font-semibold border-solid border-white hover:bg-white hover:text-black lg:text-white lg:text-2xl lg:border-white ml-6' >Lets Talk</a>
        </div>
-       <div>
-       <a href="https://www.linkedin.com/in/dan-njuguna-7a48a9235/" className='text-white' target="_blank"> <BsLinkedin size={30} /> </a>
-        <a href="https://github.com/DANNJUGUNA" className='text-white' target="_blank"> <FaGithub  size={30}/> </a>
-        <a href="" target="_blank" className='text-white'>  <AiFillTwitterCircle size={30} /> </a>
-       </div>
+      
     </div>
+     <div className='pt-5 w-[3%]'>
+     <a href="https://www.linkedin.com/in/dan-njuguna-7a48a9235/" className='text-white' target="_blank"> <BsLinkedin size={30} /> </a>
+      <a href="https://github.com/DANNJUGUNA" className='text-white' target="_blank"> <FaGithub  size={30}/> </a>
+      <a href="" target="_blank" className='text-white'>  <AiFillTwitterCircle size={30} /> </a>
+     </div>
+     </div>
   )
 }
 
