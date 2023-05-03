d1ccd29096d4ac06d94a137ec20f24ab3ac80ddd
DANNJUGUNA <ndan30071@gmail.com>
2023-05-03 13:26:29 +0300

    updated the code

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 import Home from "./components/Home";
 import NavBar from "./components/NavBar";
+import About from"./components/About";
+import Projects from"./components/Projects"
 function App() {
   return (
-    <div>
+    <div className="app">
       <NavBar/>
       <Home/>
+      <About/>
+      <Projects/>
     </div>
   );
 }
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
-
+import React from 'react';
+import dan from "../assets/dan.jpeg";
+import {BsLinkedin} from 'react-icons/bs'
+import {FaGithub} from 'react-icons/fa'
+import {AiFillTwitterCircle} from 'react-icons/ai'
+import CV from "../assets/dan resume.docx"
 const Home = () => {
+ 
   return (
-    <div>Home</div>
+    <div className=' flex-wrap items-center justify-center flex flex-col mt-0'>
+       <p className=' pt-14 mb-6 text-2xl font-semibold text-white'>
+        Hi, I'm
+       </p>
+       <p className='mb-6 text-2xl font-semibold text-white'>
+        Dan Njuguna
+       </p>
+       <p className='mb-6 text-2xl font-semibold text-white'>
+        A fullstack developer
+       </p>
+       <img className=' w-[400px] h-[400px] rounded-full' src={dan} alt="dan" />
+       <div>
+       <a href={CV}download className='text-white rounded-full p-2 border border-solid border-white'>Download CV</a>
+       <a href="#contacts" className='text-white rounded-full p-2 border border-solid border-white ml-5' >Lets Talk</a>
+       </div>
+       <div>
+       <a href="https://www.linkedin.com/in/dan-njuguna-7a48a9235/" className='text-white' target="_blank"> <BsLinkedin size={30} /> </a>
+        <a href="https://github.com/DANNJUGUNA" className='text-white' target="_blank"> <FaGithub  size={30}/> </a>
+        <a href="" target="_blank" className='text-white'>  <AiFillTwitterCircle size={30} /> </a>
+       </div>
+    </div>
   )
 }
 
